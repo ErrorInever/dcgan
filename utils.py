6655793e47b819819ecf9ed01fa113dd6c6a6e98
ErrorInever <errorinever@gmail.com>
2020-09-05 18:36:43 +0000

    fix invalid type

--- utils.py
+++ utils.py
@@ -9,11 +9,11 @@ def ones_target(size, device):
     """
     Tensor containing ones, with shape = size
     """
-    return torch.full(size, 1., dtype=torch.float, device=device)
+    return torch.full((size,), 1., dtype=torch.float, device=device)
 
 
 def zeros_target(size, device):
     """
     Tensor containing zeros, with shape = size
     """
-    return torch.full(size, 0., dtype=torch.float, device=device)
+    return torch.full((size,), 0., dtype=torch.float, device=device)
